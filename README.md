0698dcacef6a684ef790c9069f24541ded78e291
권지원 <sij411@naver.com>
2022-08-19 02:06:56 +0900

    little turtle performing
    I performed it

---
+++ README.md
@@ -0,0 +1,3 @@
+How to think like a computer scientist: Learning w python 3
+
+exercises
---
+++ little-turtles/little-turtles.py
@@ -0,0 +1,13 @@
+# 3.1 Our first turtle program
+
+import turtle
+
+wn = turtle.Screen()
+alex = turtle.Turtle()
+
+alex.forowards(50)
+alex.left(90)
+alex.forward(30)
+
+wn.mainloop()
+# This program didn't work in this VSC, so I performed it on the IDLE python..It was so uncomfortable!
