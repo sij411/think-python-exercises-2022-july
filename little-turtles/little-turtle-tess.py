466707efcec41d24e3e5f4d17f1a21ac4e43f84c
권지원 <sij411@naver.com>
2022-08-20 15:10:25 +0900

    tess-extension
    upgrade version

--- little-turtles/little-turtle-tess.py
+++ little-turtles/little-turtle-tess.py
@@ -1,11 +1,21 @@
+
+bg_color = input("What bg-color do you want to change?:", )
+tess_color = input("What color do you want to change?:", )
+tess_size = int(input("What size?:", ))
+
+
+
 import turtle
 wn = turtle.Screen()
-wn.bgcolor("lightgreen")
+
+
+
+wn.bgcolor(bg_color)
 wn.title("Hello, Tess!")
 
 tess = turtle.Turtle()
-tess.color("blue")
-tess.pensize(3)
+tess.color(tess_color)
+tess.pensize(tess_size)
 
 tess.forward(50)
 tess.left(120)
